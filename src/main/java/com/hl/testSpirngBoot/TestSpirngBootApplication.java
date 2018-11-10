feb234d54da78c22d9977e2bebdac7289ca4e600
Administrator <Administrator@JF005010>
2018-11-10 12:49:17 +0800

    初始化

---
+++ src/main/java/com/hl/testSpirngBoot/TestSpirngBootApplication.java
@@ -0,0 +1,12 @@
+package com.hl.testSpirngBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpirngBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpirngBootApplication.class, args);
+	}
+}
